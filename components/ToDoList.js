97c2fc24386d2900d090049a8772bd19637fd48e
Alena Pferdekemper <alena.pferdekemper@web.de>
2023-05-15 12:28:00 +0200

    implement delete function for todos

--- components/ToDoList.js
+++ components/ToDoList.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { StyledList } from "./StyledList";
+import { StyledEditDeleteButton } from "./StyledButtons";
+import { TiDelete } from "react-icons/ti";
 
 const StyledTodoListElement = styled.li`
   text-decoration: ${(props) => (props.completed ? "line-through" : "none")};
@@ -21,20 +23,32 @@ export default function ToDoList({ todos, setTodos }) {
     setTodos(newTodos);
   }
 
+  function handleDeleteClick(todoToDelete) {
+    setTodos(todos.filter((todo) => todo.id !== todoToDelete.id));
+  }
+
   return (
     <StyledList>
       {todos.map((todo) => {
         return (
-          <StyledTodoListElement key={todo.id} completed={todo.completed}>
-            <label htmlFor="todo">{todo?.todo}</label>
-            <input
-              type="checkbox"
-              checked={todo.completed}
-              onChange={() => handleTodoToggle(todo.id)}
-              id="todo"
-              name="todo"
-            />
-          </StyledTodoListElement>
+          <>
+            <StyledTodoListElement key={todo.id} completed={todo.completed}>
+              <label htmlFor="todo">{todo?.todo}</label>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => handleTodoToggle(todo.id)}
+                id="todo"
+                name="todo"
+              />
+              <StyledEditDeleteButton
+                onClick={() => handleDeleteClick(todo)}
+                icon={TiDelete}
+                ariaLabel={"delete"}
+                padding="0.2rem 0.5rem 0 0.3rem"
+              />
+            </StyledTodoListElement>
+          </>
         );
       })}
     </StyledList>
