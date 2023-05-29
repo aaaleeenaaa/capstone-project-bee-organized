dc59cf9a8da275a23b7a7ac28dddf90d49b3b17b
Alena Pferdekemper <alena.pferdekemper@web.de>
2023-05-29 12:26:36 +0200

    implement delete function for next colony checks

--- pages/todolist.js
+++ pages/todolist.js
@@ -1,11 +1,27 @@
 import ToDoList from "@/components/ToDoList";
 import { StyledLink } from "@/components/StyledLinks";
 import { StyledSection, StyledRowSection } from "@/components/StyledSections";
+import { StyledEditDeleteButton } from "@/components/StyledButtons";
+import { TiDeleteOutline } from "react-icons/ti";
 
-export default function ToDoListPage({ todos, setTodos, nextColonyChecks }) {
+export default function ToDoListPage({
+  todos,
+  setTodos,
+  nextColonyChecks,
+  setNextColonyChecks,
+}) {
   const sortedChecks = nextColonyChecks.sort(
     (a, b) => new Date(a.date) - new Date(b.date)
   );
+
+  function handleDeleteClick(nextColonyCheckToDelete) {
+    setNextColonyChecks(
+      nextColonyChecks.filter(
+        (nextColonyCheck) =>
+          nextColonyCheck.colonyId !== nextColonyCheckToDelete.colonyId
+      )
+    );
+  }
 
   return (
     <>
@@ -28,6 +44,13 @@ export default function ToDoListPage({ todos, setTodos, nextColonyChecks }) {
                 {check.colonyName}
               </StyledLink>
             </p>
+            <StyledEditDeleteButton
+              onClick={() => handleDeleteClick(check)}
+              icon={TiDeleteOutline}
+              ariaLabel="delete"
+              right="-4rem"
+              padding="1rem 0 0 0"
+            />
           </StyledRowSection>
         ))}
       </StyledSection>
